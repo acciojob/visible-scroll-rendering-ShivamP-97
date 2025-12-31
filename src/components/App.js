1319a15cff128d9e1ff051fadbe28b46be5136bf
= <shivampathak19972@gmail.com>
2025-12-31 22:48:13 +0530

    Test

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,68 @@
+import React, { useRef, useState } from "react";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
+const TOTAL_ITEMS = 100;
+const VISIBLE_ITEMS = 10;
+const BASE_ITEM_HEIGHT = 50;
 
 const App = () => {
+  const scrollRef = useRef(null);
+  const [scrollTop, setScrollTop] = useState(0);
+
+  const itemHeight =
+    BASE_ITEM_HEIGHT + Math.floor(scrollTop / 200);
+
+  const startIndex = Math.floor(scrollTop / itemHeight);
+  const endIndex = Math.min(
+    startIndex + VISIBLE_ITEMS,
+    TOTAL_ITEMS
+  );
+
+  const handleScroll = () => {
+    setScrollTop(scrollRef.current.scrollTop);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
 
-export default App
+      <div
+        ref={scrollRef}
+        onScroll={handleScroll}
+        style={{
+          height: "500px",
+          overflow: "auto"
+        }}
+      >
+        {/* Top spacer */}
+        <div style={{ height: startIndex * itemHeight }} />
+
+        {Array.from(
+          { length: endIndex - startIndex },
+          (_, i) => startIndex + i
+        ).map((index) => (
+          <h2
+            key={index}
+            style={{
+              height: `${itemHeight}px`,
+              margin: 0
+            }}
+          >
+            Item {index + 1}
+          </h2>
+        ))}
+
+        {/* Bottom spacer */}
+        <div
+          style={{
+            height:
+              (TOTAL_ITEMS - endIndex) *
+              itemHeight
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
